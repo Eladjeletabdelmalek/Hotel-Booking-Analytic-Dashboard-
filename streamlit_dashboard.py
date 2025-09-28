bb9c07b5e32c3b18ed7121d27fcf5eb12ac28e46
Abdelmalek_01 <eladjeletabdelmalek09@gmail.com>
2025-09-28 15:59:07 +0100

    added testing widgets

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -2,3 +2,32 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sns 
+import streamlit as st
+
+st.title('Analytic dashboard')
+file=st.file_uploader('choose a csv file or excel',type='csv')
+if file is not None :
+    df= pd.read_csv(file)
+    st.write('file uploaded')
+    
+st.subheader('data preview')
+st.write(df.head())
+st.subheader('data summary')
+st.write(df.describe()) 
+st.subheader('data checking')
+col=df.columns.tolist()
+sel_col=st.selectbox('select column',col)
+unique_values=df[sel_col].unique() 
+st.write(unique_values)
+selected_v=st.selectbox('select a value',unique_values)
+f_df=df[df[sel_col]==selected_v]
+st.write(f_df)
+st.subheader('data visualization')
+x=st.selectbox('select x_column',col)
+y=st.selectbox('select y_column',col)
+
+if st.button('generate'):
+    st.line_chart(f_df.set_index(x)[y])
+    
+else:
+    st.write('upload any file ...')    

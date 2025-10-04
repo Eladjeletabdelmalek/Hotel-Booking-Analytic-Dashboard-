bda87c3494256b6bea5ac17d923d22892927ae42
Abdelmalek_01 <eladjeletabdelmalek09@gmail.com>
2025-10-04 21:45:27 +0100

    added countries locations df

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -3,10 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 import streamlit as st
+import pycountry
+from countryinfo import CountryInfo
+
+
 
 st.set_page_config(
     page_title="Full Width App",
-    layout="wide"   # 👈 this makes it full width
+    layout="wide"   
 )
 
 st.title('Hotel booking dashboard')
@@ -22,20 +26,22 @@ with cols[0]:
 
 st.subheader('data summary')
 st.write(df.describe()) 
-st.subheader('data checking')
+# st.subheader('data checking')
 col=df.columns.tolist()
-sel_col=st.selectbox('select column',col)
-unique_values=df[sel_col].unique() 
-st.write(unique_values)
-selected_v=st.selectbox('select a value',unique_values)
-f_df=df[df[sel_col]==selected_v]
-st.write(f_df)
+# sel_col=st.selectbox('select column',col)
+# unique_values=df[sel_col].unique() 
+# st.write(unique_values)
+# selected_v=st.selectbox('select a value',unique_values)
+# f_df=df[df[sel_col]==selected_v]
+# st.write(f_df)
+st.subheader('The countries')
+st.write(df['country'].value_counts())
 st.subheader('data visualization')
 x=st.selectbox('select x_column',col)
 y=st.selectbox('select y_column',col)
 
 if st.button('generate'):
-    st.line_chart(f_df.set_index(x)[y])
+    st.line_chart(df.set_index(x)[y])
     
 else:
     st.write('Please , select any column ')    
